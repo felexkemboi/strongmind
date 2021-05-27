a3aa4ddd0b79af6cc93e31d3ad89368d800aa4ac
musioks <chandydavid88@gmail.com>
2021-05-27 13:00:58 +0300

    countries seeder and roles and permissions setup

---
+++ database/seeders/CountrySeeder.php
@@ -0,0 +1,28 @@
+<?php
+namespace Database\Seeders;
+use App\Models\Country;
+use File;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class CountrySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('countries')->delete();
+        $json = File::get("database/data/countries.json");
+        $data = json_decode($json);
+        foreach ($data as $obj) {
+            Country::create(array(
+                'name' => $obj->name,
+                'dialing_code' => $obj->phone_code,
+                'capital' => $obj->capital
+            ));
+        }
+    }
+}
