bf986828507acd0eab0da97357461d28bcd7567c
musioks <chandydavid88@gmail.com>
2021-06-14 13:06:36 +0300

    updated afterinstall hook with permissions

--- scripts/AfterInstallHook.sh
+++ scripts/AfterInstallHook.sh
@@ -3,6 +3,7 @@ set -e
 cd /opt/bitnami/projects/appapi
 ls
 sudo cp /opt/bitnami/projects/appapi/scripts/appapi-vhost.conf /opt/bitnami/apache2/conf/vhosts/
-sudo chown daemon:daemon /opt/bitnami/projects/appapi/storage/**/*
+sudo chown -R daemon:daemon /opt/bitnami/projects/appapi/storage/**/*
+sudo chmod -R 777 /opt/bitnami/projects/appapi/
 /opt/bitnami/php/bin/php artisan migrate --force
 sudo /opt/bitnami/ctlscript.sh restart apache
