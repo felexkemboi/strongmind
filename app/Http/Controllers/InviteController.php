b945755711e9d65ccd3234337dddad0e33ae4c4f
musioks <chandydavid88@gmail.com>
2021-06-22 09:17:40 +0300

    Added invite user functionality

--- app/Http/Controllers/InviteController.php
+++ app/Http/Controllers/InviteController.php
@@ -2,13 +2,16 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use Postmark\PostmarkClient;
+use App\Models\User;
 use Exception;
 use Illuminate\Database\QueryException;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Str;
+use Postmark\PostmarkClient;
+use Silber\Bouncer\Database\Role;
 use Symfony\Component\HttpFoundation\Response;
 
 /**
@@ -19,29 +22,67 @@ use Symfony\Component\HttpFoundation\Response;
  */
 class InviteController extends Controller
 {
-
-    public function invite(Request $request) : JsonResponse
+    /**
+     * Invite member
+     * @param Request $request
+     * @return JsonResponse
+     * @bodyParam  name string required  Name.
+     * @bodyParam  email string required  Email Address.
+     * @bodyParam  role_id integer required Role Id.
+     * @bodyParam  office_id integer required Office Id.
+     *
+     */
+    public function invite(Request $request): JsonResponse
     {
-        try {
-            info(config('mail.from.address'));
-            $email='davidmucioca@gmail.com';
-            $client = new PostmarkClient(config('postmark.token'));
-            $client->sendEmailWithTemplate(
-                config('mail.from.address'),
-                $email,
-                'user-invitation',
-                [
-                        'name' => 'david musyoka',
-                        'action_url' => 'https://skwea.com',
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email|unique:users',
+            'name' => 'required',
+            'role_id' => 'required',
+            'office_id' => 'required',
+        ]);
+        if ($validator->fails()) {
+            return $this->commonResponse(false, Arr::flatten($validator->messages()->get('*')), '', Response::HTTP_UNPROCESSABLE_ENTITY);
+        } else {
+            try {
+                $email = $request->email;
+                $name = $request->name;
+                $invite_token=hash('sha256',utf8_encode(Str::uuid()));
+                //Create user
+                $user=User::create(
+                    [
+                        'name'=>$name,
+                        'email'=>$email,
+                        'office_id'=>$request->office_id,
+                        'is_admin'=>0,
+                        'invite_accepted'=>0,
+                        'invite_id'=>$invite_token,
+                        'password'=>bcrypt(Str::random(8)),
+                        'active'=>0,
+                    ]
+                );
+                $role=Role::find($request->id);
+                if($role){
+                    \Bouncer::assign($role)->to($user);
+                }
+                $action_url=config('app.set_password_url')."?invite=$invite_token";
+                $client = new PostmarkClient(config('postmark.token'));
+                $client->sendEmailWithTemplate(
+                    config('mail.from.address'),
+                    $email,
+                    'user-invitation',
+                    [
+                        'name' => $name,
+                        'action_url' => $action_url,
                         'support_email' => config('mail.from.address')
                     ]
-            );
+                );
 
-            return $this->commonResponse(true, 'email sent successfully!', '', Response::HTTP_CREATED);
-        } catch (QueryException $ex) {
-            return $this->commonResponse(false, $ex->errorInfo[2], '', Response::HTTP_UNPROCESSABLE_ENTITY);
-        } catch (Exception $ex) {
-            return $this->commonResponse(false, $ex->getMessage(), '', Response::HTTP_UNPROCESSABLE_ENTITY);
+                return $this->commonResponse(true, 'email sent successfully!', '', Response::HTTP_CREATED);
+            } catch (QueryException $ex) {
+                return $this->commonResponse(false, $ex->errorInfo[2], '', Response::HTTP_UNPROCESSABLE_ENTITY);
+            } catch (Exception $ex) {
+                return $this->commonResponse(false, $ex->getMessage(), '', Response::HTTP_UNPROCESSABLE_ENTITY);
+            }
         }
     }
 }
