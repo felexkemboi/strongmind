45e67a549dd2f7c5ada1847aa5abbed3806be213
musioks <chandydavid88@gmail.com>
2021-06-24 17:42:54 +0300

    Set profile photo

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -13,9 +13,10 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
-use Silber\Bouncer\Database\Role;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Validator;
+use Silber\Bouncer\Database\Role;
 use Symfony\Component\HttpFoundation\Response;
 
 /**
@@ -36,6 +37,7 @@ class UserController extends Controller
         $users = User::query()->where('is_admin', '<>', 1)->paginate(10);
         return $this->commonResponse(true, 'success', UserResource::collection($users), Response::HTTP_OK);
     }
+
     /**
      * Change a user's password.
      * @group Auth
@@ -70,6 +72,7 @@ class UserController extends Controller
             return $this->commonResponse(true, 'Password has been changed', '', Response::HTTP_CREATED);
         }
     }
+
     /**
      * Change a user's profile.
      * @group Auth
@@ -106,22 +109,22 @@ class UserController extends Controller
                 return $this->commonResponse(false, Arr::flatten($validator->messages()->get('*')), '', Response::HTTP_UNPROCESSABLE_ENTITY);
             }
             $data = $validator->validated();
-            $office=Office::find($data['office_id']);
-            $timezone=Timezone::find($data['timezone_id']);
-            $role=Role::firstwhere('id',$data['role_id']);
-            if(!$office){
+            $office = Office::find($data['office_id']);
+            $timezone = Timezone::find($data['timezone_id']);
+            $role = Role::firstwhere('id', $data['role_id']);
+            if (!$office) {
                 return $this->commonResponse(false, 'Office not found!', '', Response::HTTP_NOT_FOUND);
             }
-            if(!$timezone){
+            if (!$timezone) {
                 return $this->commonResponse(false, 'Timezone not found!', '', Response::HTTP_NOT_FOUND);
             }
-            if(!$role){
+            if (!$role) {
                 return $this->commonResponse(false, 'Role not found!', '', Response::HTTP_NOT_FOUND);
             }
             auth()->user()->update($data);
             $user = auth()->user()->fresh();
             //delete existing role
-            DB::table('assigned_roles')->where('entity_id',$user->id)
+            DB::table('assigned_roles')->where('entity_id', $user->id)
                 ->delete();
             $user->assign($role->name);
             return $this->commonResponse(true, 'Profile updated successfully!', new UserResource($user), Response::HTTP_CREATED);
@@ -132,4 +135,41 @@ class UserController extends Controller
         }
     }
 
+    /**
+     * Set profile photo.
+     * @group Auth
+     * @param Request $request
+     * @return JsonResponse
+     * @bodyParam  profile_pic file required Profile photo
+     * @authenticated
+     */
+    public function setPhoto(Request $request): JsonResponse
+    {
+        $validator = Validator::make(
+            $request->all(), [
+            'profile_pic' => 'required|image',
+        ]);
+        if ($validator->fails()) {
+            return $this->commonResponse(false, Arr::flatten($validator->messages()->get('*')), '', Response::HTTP_UNPROCESSABLE_ENTITY);
+        } else {
+            try {
+                if ($request->hasFile('profile_pic')) {
+                    $documentPath = $request->file('profile_pic')->store('users', 's3');
+                } else {
+                    return $this->commonResponse(false, 'No file uploaded', '', Response::HTTP_BAD_REQUEST);
+                }
+                $user = Auth::user();
+                $user->profile_pic_url = Storage::disk('s3')->url($documentPath);
+                $user->profile_pic = basename($documentPath);
+                $user->save();
+
+                return $this->commonResponse(true, 'Profile photo set!', new UserResource($user), Response::HTTP_CREATED);
+            } catch (QueryException $ex) {
+                return $this->commonResponse(false, $ex->errorInfo[2], '', Response::HTTP_UNPROCESSABLE_ENTITY);
+            } catch (Exception $ex) {
+                return $this->commonResponse(false, $ex->getMessage(), '', Response::HTTP_UNPROCESSABLE_ENTITY);
+            }
+        }
+
+    }
 }
