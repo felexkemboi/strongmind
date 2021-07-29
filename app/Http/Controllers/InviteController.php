15b8ee38220366d05e6266531ab8c5765c1c7837
Dickens-Odera <dickensodera9@gmail.com>
2021-07-29 08:16:49 +0300

    (feat) - Resend Invite To User If Not Accepted

--- app/Http/Controllers/InviteController.php
+++ app/Http/Controllers/InviteController.php
@@ -12,6 +12,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Str;
+use Illuminate\Testing\TestResponse;
 use Postmark\PostmarkClient;
 use Silber\Bouncer\Database\Role;
 use Symfony\Component\HttpFoundation\Response;
@@ -48,6 +49,7 @@ class InviteController extends Controller
         try {
             $email = explode(',',$request->email);
             $invite_token = hash('sha256', utf8_encode(Str::uuid()));
+            $action_url = config('app.set_password_url') . "?invite=$invite_token";
             $role = Role::find($request->role_id);
             //if multiple emails are submitted
             if(count($email) > 1)
@@ -56,6 +58,16 @@ class InviteController extends Controller
             }else{
                 //Create user for a single email invite
                 $email = $request->email;
+                //resend invite if email address exists
+                $existingUser = User::firstWhere('email', $email);
+               // $this->checkUser($existingUser,$invite_token, $email, $action_url);
+                if($existingUser){
+                    if($existingUser->invite_accepted === 1){
+                        return $this->commonResponse(false,'Invite Accepted Already, No Action Required','', Response::HTTP_UNPROCESSABLE_ENTITY);
+                    }
+                    $existingUser->update(['invite_id' => $invite_token]);
+                    return $this->resendInvite($email, $action_url);
+                }
                 $user = User::create(
                     [
                         'email' => $email,
@@ -70,12 +82,11 @@ class InviteController extends Controller
                 if ($role) {
                     $user->assign($role->name);
                 }
-                $action_url = config('app.set_password_url') . "?invite=$invite_token";
                 $client = new PostmarkClient(config('postmark.token'));
                 $client->sendEmailWithTemplate(
                     config('mail.from.address'),
                    $email,
-                    'user-invitation',
+                    'user-invitation', //config('postmark.welcome_template_id')
                     [
                         'action_url' => $action_url,
                         'support_email' => config('mail.from.address')
@@ -154,7 +165,15 @@ class InviteController extends Controller
             {
                 $invite_token = hash('sha256', utf8_encode(Str::uuid()));
                 $action_url = config('app.set_password_url') . "?invite=$invite_token";
-
+                //if email exists, resend invite else add new user account
+                $existingUser = User::firstWhere('email', $member);
+                if($existingUser){
+                    if($existingUser->invite_accepted === 1){
+                        return $this->commonResponse(false,'Invite Accepted Already, No Action Required','', Response::HTTP_UNPROCESSABLE_ENTITY);
+                    }
+                    $existingUser->update(['invite_id' => $invite_token]);
+                    return $this->resendInvite($email, $action_url);
+                }
                 $user = User::create([
                     'email' => $member,
                     'office_id' => $request->office_id,
@@ -170,7 +189,7 @@ class InviteController extends Controller
                 $client->sendEmailWithTemplate(
                     config('mail.from.address'),
                     $member,
-                    config('postmark.welcome_template_id'),
+                    'user-invitation', //config('postmark.welcome_template_id')
                     [
                         'action_url' => $action_url,
                         'support_email' => config('mail.from.address')
@@ -182,6 +201,49 @@ class InviteController extends Controller
             return $this->commonResponse(false,$queryException->errorInfo[2],'', Response::HTTP_UNPROCESSABLE_ENTITY);
         }catch (Exception $exception){
             return $this->commonResponse(false, $exception->getMessage(),'', Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    /**
+     * @param $email
+     * @param $action_url
+     * @return JsonResponse
+     */
+    private function resendInvite($email, $action_url): JsonResponse
+    {
+        $client = new PostmarkClient(config('postmark.token'));
+        try{
+            $client->sendEmailWithTemplate(
+                config('mail.from.address'),
+                $email,
+                'user-invitation',
+                [
+                    'action_url' => $action_url,
+                    'support_email' => config('mail.from.address')
+                ]
+            );
+            return $this->commonResponse(true,'Invite Resent Successfully','', Response::HTTP_CREATED);
+        }catch (Exception $exception){
+            return $this->commonResponse(false,$exception->getMessage(), '', Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+    }
+
+    /**
+     * Check if user invite status before resending email
+     * @param $user
+     * @param $invite_token
+     * @param $email
+     * @param $action_url
+     * @return JsonResponse
+     */
+    private function checkUser($invite_token, $email, $action_url){
+        $user = User::firstWhere('email', $email);
+        if($user){
+            if($user->invite_accepted === 1){
+                return $this->commonResponse(false,'Invite Accepted Already, No Action Required','', Response::HTTP_UNPROCESSABLE_ENTITY);
+            }
+            $user->update(['invite_id' => $invite_token]);
+            return $this->resendInvite($email, $action_url);
         }
     }
 }
