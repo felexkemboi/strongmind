51643415e6429a34bafbff585a8f98b455bd50d4
Dickens-Odera <dickensodera9@gmail.com>
2021-08-21 16:04:52 +0300

    (Fix) - Added missing Office Model

--- app/Models/Programs/Program.php
+++ app/Models/Programs/Program.php
@@ -3,6 +3,7 @@
 namespace App\Models\Programs;
 
 use App\Models\Misc\ProgramType;
+use App\Models\Office;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
