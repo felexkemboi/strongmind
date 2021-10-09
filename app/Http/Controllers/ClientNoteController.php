10f1cee7405f2221aa604b1914251a1ac2e5ceb7
felexkemboi <felokemboi10@gmail.com>
2021-10-09 15:46:42 +0300

    add more data to the response

--- app/Http/Controllers/ClientNoteController.php
+++ app/Http/Controllers/ClientNoteController.php
@@ -4,8 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Models\Client;
 use App\Models\ClientNote;
-use App\Models\User;
-use App\Support\Collection;
 use Illuminate\Database\QueryException;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
@@ -39,13 +37,13 @@ class ClientNoteController extends Controller
             'notes' => 'required|string|min:5|max:255',
             'private' => 'required|boolean'
         ]);
-        if($validator->fails()){
-            return $this->commonResponse(false,Arr::flatten($validator->messages()->get('*')),'', Response::HTTP_UNPROCESSABLE_ENTITY);
+        if ($validator->fails()) {
+            return $this->commonResponse(false, Arr::flatten($validator->messages()->get('*')), '', Response::HTTP_UNPROCESSABLE_ENTITY);
         }
-        try{
-            $client = Client::with('timezone','country','status','channel','staff')->find($id);
-            if(!$client){
-                return $this->commonResponse(false,'Client Does Not Exist','', Response::HTTP_NOT_FOUND);
+        try {
+            $client = Client::with('timezone', 'country', 'status', 'channel', 'staff')->find($id);
+            if (!$client) {
+                return $this->commonResponse(false, 'Client Does Not Exist', '', Response::HTTP_NOT_FOUND);
             }
             $clientNoteData = [
                 'client_id' => $client->id,
@@ -53,15 +51,15 @@ class ClientNoteController extends Controller
                 'private'  => $request->private,
                 'notes'    => $request->notes
             ];
-            if(ClientNote::create($clientNoteData)){
-                return $this->commonResponse(true,'Client notes created successfully','', Response::HTTP_OK);
+            if (ClientNote::create($clientNoteData)) {
+                return $this->commonResponse(true, 'Client notes created successfully', '', Response::HTTP_OK);
             }
-            return $this->commonResponse(false,'Client notes not created','', Response::HTTP_UNPROCESSABLE_ENTITY);
-        }catch (QueryException $queryException){
-            return $this->commonResponse(false,$queryException->errorInfo[2],'', Response::HTTP_UNPROCESSABLE_ENTITY);
-        }catch (Exception $exception){
-            Log::critical('Could Not Create Client Notes. ERROR: '.$exception->getTraceAsString());
-            return $this->commonResponse(false,$exception->getMessage(),'', Response::HTTP_INTERNAL_SERVER_ERROR);
+            return $this->commonResponse(false, 'Client notes not created', '', Response::HTTP_UNPROCESSABLE_ENTITY);
+        } catch (QueryException $queryException) {
+            return $this->commonResponse(false, $queryException->errorInfo[2], '', Response::HTTP_UNPROCESSABLE_ENTITY);
+        } catch (Exception $exception) {
+            Log::critical('Could Not Create Client Notes. ERROR: ' . $exception->getTraceAsString());
+            return $this->commonResponse(false, $exception->getMessage(), '', Response::HTTP_INTERNAL_SERVER_ERROR);
         }
     }
 
@@ -74,20 +72,24 @@ class ClientNoteController extends Controller
      */
     public function getPublicNotes(int $id): JsonResponse
     {
-        try{
-            $client = Client::with('timezone','country','status','channel','staff')->find($id);
-            if(!$client){
-                return $this->commonResponse(false,'Client Does Not Exist','', Response::HTTP_NOT_FOUND);
+        try {
+            $client = Client::find($id);
+
+            if (!$client) {
+                return $this->commonResponse(false, 'Client Does Not Exist', '', Response::HTTP_NOT_FOUND);
             }
-            $publicNotes = ClientNote::public()->where(function($query) use($client){
-                $query->where('client_id', $client->id);
-            })->select('client_id','staff_id','private','notes')->latest()->paginate(10);
-            return $this->commonResponse(true,'success',$publicNotes, Response::HTTP_OK);
-        }catch (QueryException $queryException){
-            return $this->commonResponse(false,$queryException->errorInfo[2],'', Response::HTTP_UNPROCESSABLE_ENTITY);
-        }catch (Exception $exception){
-            Log::critical('Unable to fetch client public notes. ERROR: '.$exception->getTraceAsString());
-            return $this->commonResponse(false,$exception->getMessage(),'', Response::HTTP_INTERNAL_SERVER_ERROR);
+            $publicNotes = ClientNote::public()
+                ->where(fn ($query) => $query->where('client_id', $client->id))
+                ->with(['staff' => fn ($query) => $query->select('id', 'name', 'profile_pic_url')])
+                ->select('created_at', 'staff_id', 'notes', 'id')
+                ->latest();
+
+            return $this->commonResponse(true, 'success', $publicNotes, Response::HTTP_OK);
+        } catch (QueryException $queryException) {
+            return $this->commonResponse(false, $queryException->errorInfo[2], '', Response::HTTP_UNPROCESSABLE_ENTITY);
+        } catch (Exception $exception) {
+            Log::critical('Unable to fetch client public notes. ERROR: ' . $exception->getTraceAsString());
+            return $this->commonResponse(false, $exception->getMessage(), '', Response::HTTP_INTERNAL_SERVER_ERROR);
         }
     }
 
@@ -101,22 +103,24 @@ class ClientNoteController extends Controller
      */
     public function getPrivateNotes(Request $request, int $id): JsonResponse
     {
-        try{
-            $client = Client::with('timezone','country','status','channel','staff')->find($id);
-            if(!$client){
-                return $this->commonResponse(false,'Client Does Not Exist','', Response::HTTP_NOT_FOUND);
+        try {
+            $client = Client::find($id);
+            if (!$client) {
+                return $this->commonResponse(false, 'Client Does Not Exist', '', Response::HTTP_NOT_FOUND);
             }
-            $privateNotes = ClientNote::private($request)->where(function($query) use($client){
-                $query->where('client_id', $client->id);
-            })->select('client_id','staff_id','private','notes')
-                ->latest()
-                ->paginate(10);
-            return $this->commonResponse(true,'success', $privateNotes, Response::HTTP_OK);
-        }catch (QueryException $queryException){
-            return $this->commonResponse(false,$queryException->errorInfo[2],'', Response::HTTP_UNPROCESSABLE_ENTITY);
-        }catch (Exception $exception){
-            Log::critical('Unable to fetch client private notes. ERROR: '.$exception->getTraceAsString());
-            return $this->commonResponse(false,$exception->getMessage(),'', Response::HTTP_INTERNAL_SERVER_ERROR);
+
+            $privateNotes =  ClientNote::private($request)
+                ->where(fn ($query) => $query->where('client_id', $client->id))
+                ->with(['staff' => fn ($query) => $query->select('id', 'name', 'profile_pic_url')])
+                ->select('created_at', 'staff_id', 'notes', 'id')
+                ->latest();
+
+            return $this->commonResponse(true, 'success', $privateNotes, Response::HTTP_OK);
+        } catch (QueryException $queryException) {
+            return $this->commonResponse(false, $queryException->errorInfo[2], '', Response::HTTP_UNPROCESSABLE_ENTITY);
+        } catch (Exception $exception) {
+            Log::critical('Unable to fetch client private notes. ERROR: ' . $exception->getTraceAsString());
+            return $this->commonResponse(false, $exception->getMessage(), '', Response::HTTP_INTERNAL_SERVER_ERROR);
         }
     }
 }
