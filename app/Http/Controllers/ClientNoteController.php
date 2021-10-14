217124bfff8ab9a9cb8749fe59cd8f10d1733ba8
felexkemboi <felokemboi10@gmail.com>
2021-10-14 16:39:16 +0300

    add edit/delete note button

--- app/Http/Controllers/ClientNoteController.php
+++ app/Http/Controllers/ClientNoteController.php
@@ -9,8 +9,11 @@ use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Log;
-use Illuminate\Support\Facades\Validator;
 use Symfony\Component\HttpFoundation\Response;
+use App\Http\Requests\CreateNoteRequest;
+use App\Http\Requests\EditNoteRequest;
+use Illuminate\Support\Facades\Auth;
+
 use Exception;
 
 /**
@@ -23,7 +26,7 @@ class ClientNoteController extends Controller
 {
     /**
      * Add Client Notes
-     * @param Request $request
+     * @param NoteRequest $request
      * @param int $id
      * @urlParam id integer required . The Client ID
      * @bodyParam private boolean required . Specify whether true or false
@@ -31,17 +34,10 @@ class ClientNoteController extends Controller
      * @return JsonResponse
      * @authenticated
      */
-    public function create(Request $request, int $id): JsonResponse
+    public function create(CreateNoteRequest $request, int $id): JsonResponse
     {
-        $validator = Validator::make($request->all(), [
-            'notes' => 'required|string|min:5|max:255',
-            'private' => 'required|boolean'
-        ]);
-        if ($validator->fails()) {
-            return $this->commonResponse(false, Arr::flatten($validator->messages()->get('*')), '', Response::HTTP_UNPROCESSABLE_ENTITY);
-        }
         try {
-            $client = Client::with('timezone', 'country', 'status', 'channel', 'staff')->find($id);
+            $client = Client::find($id);
             if (!$client) {
                 return $this->commonResponse(false, 'Client Does Not Exist', '', Response::HTTP_NOT_FOUND);
             }
@@ -52,6 +48,10 @@ class ClientNoteController extends Controller
                 'notes'    => $request->notes
             ];
             if (ClientNote::create($clientNoteData)) {
+                activity('client')
+                    ->performedOn($client)
+                    ->causedBy($request->user()->id)
+                    ->log('Note for '.$client->name.' created');
                 return $this->commonResponse(true, 'Client notes created successfully', '', Response::HTTP_OK);
             }
             return $this->commonResponse(false, 'Client notes not created', '', Response::HTTP_UNPROCESSABLE_ENTITY);
@@ -111,6 +111,66 @@ class ClientNoteController extends Controller
         } catch (Exception $exception) {
             Log::critical('Unable to fetch client private notes. ERROR: ' . $exception->getTraceAsString());
             return $this->commonResponse(false, $exception->getMessage(), '', Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    /**
+     * Edit Client Note
+     * @param EditNoteRequest $request
+     * @param int $id
+     * @urlParam id integer required . The Client ID
+     * @bodyParam private boolean required . Specify whether true or false
+     * @bodyParam notes string required . The specific notes about this client
+     * @bodyParam client_id int required . The client the note belongs to
+     * @bodyParam staff_id int required . The staff the note is responsible for
+     * @return JsonResponse
+     * @authenticated
+     */
+    public function edit(EditNoteRequest $request, int $id): JsonResponse
+    {
+        try {
+            if (!ClientNote::find($id)) {
+                return $this->commonResponse(false, 'Note Does Not Exist', '', Response::HTTP_NOT_FOUND);
+            }
+
+            if(ClientNote::find($id)->update([ 'client_id' => $request->client, 'staff_id'  => $request->staff,'private'  => $request->private,'notes'    => $request->notes])){
+                return $this->commonResponse(true, 'Note updated successfully', '', Response::HTTP_OK);
+            }
+            return $this->commonResponse(false, 'Note not updated', '', Response::HTTP_UNPROCESSABLE_ENTITY);
+        } catch (QueryException $queryException) {
+            return $this->commonResponse(false, $queryException->errorInfo[2], '', Response::HTTP_UNPROCESSABLE_ENTITY);
+        } catch (Exception $exception) {
+            Log::critical('Could Not Update the note. ERROR: ' . $exception->getTraceAsString());
+            return $this->commonResponse(false, $exception->getMessage(), '', Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    /**
+     * Delete  Note
+     * @param Request $request
+     * @param int $id
+     * @urlParam id integer required . The Note ID
+     * @return JsonResponse
+     * @authenticated
+     */
+    public function destroy($id)
+    {
+
+        try{
+            \Log::debug($id);
+            $note = ClientNote::find($id);
+            if(!$note){
+                return $this->commonResponse(false,'Note Does Not Exist','', Response::HTTP_NOT_FOUND);
+            }
+            if($note->delete()){
+                return $this->commonResponse(true,'Note Deleted Successfully', '', Response::HTTP_OK);
+            }
+            return$this->commonResponse(false,'Failed To Delete Note','', Response::HTTP_EXPECTATION_FAILED);
+        }catch (QueryException $queryException){
+            return $this->commonResponse(false,$queryException->errorInfo[2], '', Response::HTTP_UNPROCESSABLE_ENTITY);
+        }catch (Exception $exception){
+            Log::critical('Could Not Delete Note. ERROR:  '.$exception->getTraceAsString());
+            return $this->commonResponse(false,$exception->getMessage(), '', Response::HTTP_INTERNAL_SERVER_ERROR);
         }
     }
 }
