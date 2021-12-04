ac21efa864c67c9ab59fa6c7a39174202d0a39c8
Dickens-Odera <dickensodera9@gmail.com>
2021-12-04 16:03:40 +0300

    feat: initial implementation of group sessions

--- app/Actions/GroupSessionAction.php
+++ app/Actions/GroupSessionAction.php
@@ -5,8 +5,45 @@ namespace App\Actions;
 
 
 use App\Traits\ApiResponser;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 
 class GroupSessionAction
 {
     use ApiResponser;
+
+    public function listSessions(): JsonResponse
+    {
+
+    }
+
+    public function createSession(): JsonResponse
+    {
+
+    }
+
+    public function viewSessionDetails(int $id): JsonResponse
+    {
+
+    }
+
+    public function updateSession(Request $request, int $id): JsonResponse
+    {
+
+    }
+
+    public function deleteSession(int $id): JsonResponse
+    {
+
+    }
+
+    public function recordSessionAttendance(int $id): JsonResponse
+    {
+
+    }
+
+    public function listSessionAttendance(int $id): JsonResponse
+    {
+
+    }
 }
--- app/Http/Controllers/Groups/GroupSessionController.php
+++ app/Http/Controllers/Groups/GroupSessionController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers\Groups;
 
+use App\Actions\GroupSessionAction;
 use App\Http\Controllers\Controller;
+use App\Services\PermissionRoleService;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
@@ -13,6 +15,14 @@ use Illuminate\Http\Request;
  */
 class GroupSessionController extends Controller
 {
+    public $permissionRoleService;
+    public $groupSessionAction;
+
+    public function __construct(PermissionRoleService $permissionRoleService, GroupSessionAction $groupSessionAction){
+        $this->permissionRoleService = $permissionRoleService;
+        $this->groupSessionAction = $groupSessionAction;
+    }
+
     /**
      * List Group Sessions
      *
@@ -20,7 +30,8 @@ class GroupSessionController extends Controller
      */
     public function index(): JsonResponse
     {
-        //
+        $this->permissionRoleService->verifyUserHasPermissionTo('list groups with their respective sessions');
+        return $this->groupSessionAction->listSessions();
     }
 
     /**
