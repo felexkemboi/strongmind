9fb8d04f7ead1a11a890ba4acf01b0e0a3f33d20
felexkemboi <felokemboi10@gmail.com>
2022-01-10 13:38:24 +0300

    seed field types

--- database/seeders/FieldTypeSeeder.php
+++ database/seeders/FieldTypeSeeder.php
@@ -17,40 +17,49 @@ class FieldTypeSeeder extends Seeder
      */
     public function run()
     {
-
         DB::table('field_type')->truncate();
         $types = array(
             array(
                 'name' => 'Numeric',
                 'data_type' => 'numeric',
+                'icon' => 'HashtagIcon',
                 'created_at' => Carbon::now(),
                 'updated_at' => Carbon::now(),
             ),
             array(
                 'name' => 'Text',
                 'data_type' => 'text',
+                'icon' => 'MenuAlt2Icon',
                 'created_at' => Carbon::now(),
                 'updated_at' => Carbon::now(),
             ),
             array(
-                'name' => 'Multiple',
+                'name' => 'Multiple choice',
                 'data_type' => 'multiple',
+                'icon' =>'MenuIcon',
                 'created_at' => Carbon::now(),
                 'updated_at' => Carbon::now(),
             ),
             array(
                 'name' => 'Date Time',
                 'data_type' => 'datetime',
+                'icon' => 'PresentationChartLineIcon',
                 'created_at' => Carbon::now(),
                 'updated_at' => Carbon::now(),
             ),
             array(
                 'name' => 'Date',
                 'data_type' => 'date',
+                'icon' => 'PresentationChartLineIcon',
                 'created_at' => Carbon::now(),
                 'updated_at' => Carbon::now(),
             )
         );
-        FieldType::insert($types);
+        // FieldType::insert($types);
+        foreach ($types as $obj) {
+            FieldType::create(
+                $obj
+            );
+        }
     }
 }
