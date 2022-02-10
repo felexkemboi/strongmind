3a3d34a116c53957d5174b73939fab948dbaccc4
felexkemboi <felokemboi10@gmail.com>
2022-02-10 14:16:13 +0300

    update in changing the column type

--- database/migrations/2022_02_10_084519_alter_table_client_form_change_status_id.php
+++ database/migrations/2022_02_10_084519_alter_table_client_form_change_status_id.php
@@ -3,6 +3,8 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
 
 class AlterTableClientFormChangeStatusId extends Migration
 {
@@ -14,7 +16,8 @@ class AlterTableClientFormChangeStatusId extends Migration
     public function up()
     {
         Schema::table('client_form', function (Blueprint $table) {
-            $table->integer('status_id')->change();
+            // $table->integer('status_id')->change();
+            DB::statement('ALTER TABLE client_form ALTER COLUMN status_id TYPE integer USING (status_id)::integer');
         });
     }
 
