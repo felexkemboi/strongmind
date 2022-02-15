e156ced81234a2f74b119340b565a9e55f6b0277
felexkemboi <felokemboi10@gmail.com>
2022-02-15 08:26:16 +0300

    add email to client biodata

--- app/Http/Controllers/FormController.php
+++ app/Http/Controllers/FormController.php
@@ -218,7 +218,7 @@ class FormController extends Controller
 
             $payload = array();
             foreach ($clients as $client) {
-                $clientDetails   = ClientBioData::select('first_name','last_name')->firstWhere('client_id', $client['client_id']);
+                $clientDetails   = ClientBioData::select('first_name','last_name','email')->firstWhere('client_id', $client['client_id']);
                 $responses = QuestionResponses::where('form_id', $form->id)->where('client_id', $client['client_id'])->get();
                 $clientResponses = array();
                 foreach ($responses as $response) {
