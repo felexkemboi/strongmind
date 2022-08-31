a2ce70431c47e30b1791a31955834eb1177928d8
Dennis Muthuri <muthuridennis@gmail.com>
2022-08-31 17:23:39 +0300

    Change temporary creation path

--- config/excel.php
+++ config/excel.php
@@ -293,7 +293,7 @@ return [
         | storing reading or downloading. Here you can customize that path.
         |
         */
-        'local_path'          => storage_path('imported'),
+        'local_path'          => storage_path('app/public'),
 
         /*
         |--------------------------------------------------------------------------
